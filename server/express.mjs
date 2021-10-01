4a969bf90f0faf33b391a9e0af8702374907ffc6
yehiel <yehiel9523@gmail.com>
2021-10-01 15:05:32 +0300

    change port

--- server/express.mjs
+++ server/express.mjs
@@ -60,5 +60,5 @@ app.put('/products/:id', async(req, res) => {
         res.send(err.message)
     }
 })
-const PORT = process.env.PORT || 8000;
-app.listen(PORT);
+const port = process.env.PORT || 8000;
+app.listen(port);
